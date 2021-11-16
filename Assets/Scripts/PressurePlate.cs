77462871c9a68bd0949bc50ad160fb44242bc146
Liam Forsberg <fourzifer@gmail.com>
2021-11-16 16:14:22 +0100

    3rd pressure plate (v2)

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -7,17 +7,14 @@ public class PressurePlate : MonoBehaviour
     private bool switchIsActive;
     [SerializeField] private GameObject door;
     [SerializeField] private int switchType;
-    public static int totalPlayers;
+    public static int totalPlayers = 0;
 
     private void Update()
     {
-        /*requires 2 pressure plates
-        (a known bug is that two players could stand on the same pressure plate)*/
-        if (totalPlayers == 2 && door != null)
+        //(a known "bug" is that two players could stand on the same pressure plate)
+        if (totalPlayers == 2 && switchType == 3 && door != null)
         {
-            Debug.Log("Door destroyed");
-            Destroy(door);
-            totalPlayers = 0;
+            door.SetActive(false);
         }
     }
 
@@ -29,13 +26,11 @@ public class PressurePlate : MonoBehaviour
             {
                 case 1:
                     switchIsActive = true;
-                    Debug.Log("Opened");
                     door.SetActive(false);
                     return;
                 case 2:
                     switchIsActive = true;
-                    Debug.Log("Door destroyed");
-                    Destroy(door);
+                    door.SetActive(false);
                     return;
                 case 3:
                     totalPlayers++;
@@ -51,17 +46,20 @@ public class PressurePlate : MonoBehaviour
     {
         switch (switchType)
         {
+            case 1:
+                if (other.gameObject.tag == "Player" && door != null)
+                {
+                    switchIsActive = false;
+                    door.SetActive(true);
+                }
+                return;
+            case 2:
+                return;
             case 3:
                 totalPlayers--;
                 Debug.Log(totalPlayers);
                 return;
             default:
-                if (other.gameObject.tag == "Player" && door != null)
-                {
-                    switchIsActive = false;
-                    Debug.Log("Closed");
-                    door.SetActive(true);
-                }
                 return;
         }
     }
